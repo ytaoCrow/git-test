9490d78dddd13d8979cf3b4eaa41fd7f9afd4f3c
Justin <kafka880407@gmail.com>
2022-09-03 11:56:43 +0800

    try push

---
+++ gitProject/src/main/java/com/example/gitproject/GitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitProjectApplication.class, args);
+    }
+
+}
